d27de987ea004a82a9a046c0a10e2a765a8c9c5f
Pavel Sotnikov <pavel.sotnikov.dne@gmail.com>
2016-09-15 23:41:07 +0300

    ConnectionType added (+enum)

--- NewContext.cs
+++ NewContext.cs
@@ -9,4 +9,18 @@ public class Sensor
 {
  public int sensorID {get; set;}
  public string parametrDescription {get; set;}
+ 
+ public ConnectionType {get; set;}
+
 }
+
+
+enum ConnectionType
+{
+ USB,
+ RS232,
+ RS485,
+ Wireless
+}
+
+
